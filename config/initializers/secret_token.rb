a12a8ce623f28c6316f0ea7d64fffae4834eef91
ggomagundan <ggogun@gmail.com>
2014-08-12 08:27:30 +0900

    basic commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+UnlimitedImage::Application.config.secret_key_base = '47f534496c6ffe5d94bcf06884b656d10bca5fa04e14ffc057ee7325ba716f4b4b5790cb9f659cd4b89a9f366af7a8b75102cef7fc5d0907f37601d594054105'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+UnlimitedImage::Application.config.session_store :cookie_store, key: '_unlimited_image_session'
