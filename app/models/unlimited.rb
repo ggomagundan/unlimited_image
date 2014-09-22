3cc3e331c202831ea550c0cfffb1bd92637339cf
ggomagundan <ggogun@gmail.com>
2014-09-23 00:45:03 +0900

    modify file

--- app/models/unlimited.rb
+++ app/models/unlimited.rb
@@ -6,5 +6,23 @@ class Unlimited
   field :is_visible, type: Integer, default: 1
   field :original_url, type: String
   field :image_url, type: String
+  field :owner_name, type: String
+  field :owner_link, type: String
+  field :image_source, type: String
+  field :cc_code, type: Integer, default: 0
+
+
+  # http://creativecommons.org/licenses/
+  CC_NAME =[
+     "None",
+     "CC BY",
+     "CC BY-ND",
+     "CC BY-SA"
+  ]
+
+  def cc_str
+    Unlimited::CC_NAME[self.cc_code]
+  end
+
 
 end
--- lib/five00px.rb
+++ lib/five00px.rb
@@ -1,11 +1,11 @@
 # -*- encoding : utf-8 -*-
 class Five00px
 
-  def five_px(page)
+  def five_px(pages)
 
     require 'open-uri'
     require 'nokogiri'
-    url = "http://500px.com/search?utf8=%E2%9C%93&q=&type=photos&order=votes&license_type=4&page=#{page}"
+    url = "http://500px.com/search?utf8=%E2%9C%93&q=&type=photos&order=votes&license_type=4&page=#{pages}" # CC BY
     page = Nokogiri::HTML(open(url))
     page.css("div.container div.col.d4")
     @link_arr= []
@@ -13,11 +13,45 @@ class Five00px
       original_url = link.css("a")[0]["href"].to_s
       if Unlimited.where(original_url: original_url).blank?
         img_url = real_500px_image(link.css("a")[0]["href"])
-        if img_url != "none"
-          Unlimited.create(original_url: original_url, image_url: img_url)
+        owner_name, owner_link = get_owner_name(link.css("a")[0]["href"])
+        if img_url != "none" && Unlimited.where(image_url: img_url).blank?
+          Unlimited.create(original_url: original_url, image_url: img_url, owner_name: owner_name, owner_link: owner_link, image_source: "500px", cc_code: 1)
         end
       end
     end
+
+
+    url = "http://500px.com/search?utf8=%E2%9C%93&q=&type=photos&order=votes&license_type=5&page=#{pages}" # CC BY ND
+    page = Nokogiri::HTML(open(url))
+    page.css("div.container div.col.d4")
+    @link_arr= []
+    page.css("div.container div.col.d4").each do |link|
+      original_url = link.css("a")[0]["href"].to_s
+      if Unlimited.where(original_url: original_url).blank?
+        img_url = real_500px_image(link.css("a")[0]["href"])
+        owner_name, owner_link = get_owner_name(link.css("a")[0]["href"])
+        if img_url != "none" && Unlimited.where(image_url: img_url).blank?
+          Unlimited.create(original_url: original_url, image_url: img_url, owner_name: owner_name, owner_link: owner_link, image_source: "500px", cc_code: 2)
+        end
+      end
+    end
+
+    url = "http://500px.com/search?utf8=%E2%9C%93&q=&type=photos&order=votes&license_type=6&page=#{pages}" # CC BY SA
+    page = Nokogiri::HTML(open(url))
+    page.css("div.container div.col.d4")
+    @link_arr= []
+    page.css("div.container div.col.d4").each do |link|
+      original_url = link.css("a")[0]["href"].to_s
+      if Unlimited.where(original_url: original_url).blank?
+        img_url = real_500px_image(link.css("a")[0]["href"])
+        owner_name, owner_link = get_owner_name(link.css("a")[0]["href"])
+        if img_url != "none" && Unlimited.where(image_url: img_url).blank?
+          Unlimited.create(original_url: original_url, image_url: img_url, owner_name: owner_name, owner_link: owner_link, image_source: "500px", cc_code: 3)
+        end
+      end
+    end
+
+
 
   end
 
@@ -33,6 +67,22 @@ class Five00px
     rescue => e
       puts e
       return "none"
+    end
+  end
+ 
+  def get_owner_name(append_url)
+
+   
+    begin
+      puts "http://500px.com#{append_url}"
+      url = "http://500px.com#{append_url}"
+      page = Nokogiri::HTML(open(url))
+      owner_name = page.css(".author_wrap .author_name a")[0].text
+      owner_link= page.css(".author_wrap .author_name a")[0]["href"]
+      [owner_name, owner_link]
+    rescue => e
+      puts e
+      return ["none", "none"]
     end
   end
  
