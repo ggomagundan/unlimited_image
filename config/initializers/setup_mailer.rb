38a333ad12e73cf9214ca46869683e964404991d
ggomagundan <ggogun@gmail.com>
2014-09-26 02:33:07 +0900

    add mailer

---
+++ config/initializers/setup_mailer.rb
@@ -0,0 +1,21 @@
+# -*- encoding : utf-8 -*-
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.perform_deliveries = true
+ActionMailer::Base.raise_delivery_errors = true
+ActionMailer::Base.smtp_settings = {
+  :address => "smtp.gmail.com",
+  :port => 587,
+  :domain => "randomImage.com",
+  :user_name => "limuz12188.com",
+  :password => "q0w9e8r7",
+  :authentication => "login",
+  :enable_starttls_auto => true
+
+#  :address => '127.0.0.1',
+#  :port => 25,
+#  :doamin => 'priday.com',
+#  :enable_starttls_auto => false,
+#  :openssl_verify_mode => 'none'
+
+}
+
